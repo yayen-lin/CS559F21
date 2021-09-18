5ea5dc1d3e4b8f09b95bfdcf13839f6e9bdbbcf2
Andy Lin <lin383@wisc.edu>
2021-09-18 14:12:43 -0500

    added interior to the cube

--- hw01/hw01.js
+++ hw01/hw01.js
@@ -9,38 +9,65 @@ draw = () => {
   var canvas = document.getElementById("hw01Canvas");
   var s1 = document.getElementById("slider1");
   var s2 = document.getElementById("slider2");
-  s1.value = 0;
-  s2.value = 0;
+  // s1.value = 70;
+  // s2.value = 40;
+
+  // sx = s1.value;
+  // sy = s2.value;
 
   var context = canvas.getContext("2d");
 
-  drawCube = () => {
+  drawCube = (sx, sy) => {
+    // settings of coordinate
+    var [startingX, startingY] = [sx, sy]; // starting points of (x, y) coords
+    var [x, y] = [startingX, startingY];
     context.beginPath();
 
     // top of the cube
-    var [x, y] = [50, 50]; // starting points of (x, y) coords
     context.moveTo(x, y);
     context.lineTo((x += 100), y);
     context.lineTo((x -= 50), (y += 30));
     context.lineTo((x -= 100), y);
     context.lineTo((x += 50), (y -= 30));
+    context.closePath();
 
     // face of the cube
     context.moveTo((x -= 50), (y += 30));
     context.lineTo(x, (y += 100));
     context.lineTo((x += 100), y);
     context.lineTo(x, (y -= 100));
+    // context.closePath();
 
     // right side of the cube
     console.log(x, y);
     context.moveTo(x, (y += 100));
     context.lineTo((x += 50), (y -= 30));
     context.lineTo(x, (y -= 100));
+    // context.closePath();
 
+    // stroke the solid outline of the cube
+    context.stroke();
+
+    // draw interior of the cube
+    [x, y] = [startingX, startingY];
+    context.beginPath();
+
+    // inner view of the cube
+    context.moveTo((x = startingX), (y = startingY));
+    context.setLineDash([4, 4]);
+    console.log(x, y);
+    context.lineTo(x, (y += 100));
+    context.lineTo((x -= 50), (y += 30));
+    context.moveTo((x += 50), (y -= 30));
+    context.lineTo((x += 100), y);
+
+    // stroke dotted interior line
     context.stroke();
   };
 
-  drawCube();
+  // context.save(); // save current state onto a stack
+  // drawCube(dx, dy);
+  drawCube(70, 40);
 };
 
 window.onload = () => {
