af23846e77ba1bd24304c02eb88db3db3a70fb97
Andy Lin <lin383@wisc.edu>
2021-09-18 15:30:46 -0500

    made sliders working

--- hw01/hw01.js
+++ hw01/hw01.js
@@ -4,72 +4,42 @@
  * Fall 2021 CS 559 Assignment 01
  * files: hw01.html, hw01.js
  */
-
-draw = () => {
+setup = () => {
+  // getting elements from html
   var canvas = document.getElementById("hw01Canvas");
   var s1 = document.getElementById("slider1");
   var s2 = document.getElementById("slider2");
-  // s1.value = 70;
-  // s2.value = 40;
+  s1.value = 0;
+  s2.value = 0;
 
-  // sx = s1.value;
-  // sy = s2.value;
+  draw = () => {
+    var context = canvas.getContext("2d");
+    canvas.width = canvas.width; // clear canvas before drawing
 
-  var context = canvas.getContext("2d");
+    var [startingX, startingY] = [s1.value, s2.value]; // starting points of (x, y) coords
 
-  drawCube = (sx, sy) => {
-    // settings of coordinate
-    var [startingX, startingY] = [sx, sy]; // starting points of (x, y) coords
-    var [x, y] = [startingX, startingY];
-    context.beginPath();
+    drawCube = () => {
+      // settings of coordinate
+      var [x, y] = [startingX, startingY];
+      context.beginPath();
 
-    // top of the cube
-    context.moveTo(x, y);
-    context.lineTo((x += 100), y);
-    context.lineTo((x -= 50), (y += 30));
-    context.lineTo((x -= 100), y);
-    context.lineTo((x += 50), (y -= 30));
-    context.closePath();
+      // top of the cube
+      context.moveTo(x, y);
+      context.lineTo(100, 100);
 
-    // face of the cube
-    context.moveTo((x -= 50), (y += 30));
-    context.lineTo(x, (y += 100));
-    context.lineTo((x += 100), y);
-    context.lineTo(x, (y -= 100));
-    // context.closePath();
+      // context.closePath();
 
-    // right side of the cube
-    console.log(x, y);
-    context.moveTo(x, (y += 100));
-    context.lineTo((x += 50), (y -= 30));
-    context.lineTo(x, (y -= 100));
-    // context.closePath();
-
-    // stroke the solid outline of the cube
-    context.stroke();
-
-    // draw interior of the cube
-    [x, y] = [startingX, startingY];
-    context.beginPath();
-
-    // inner view of the cube
-    context.moveTo((x = startingX), (y = startingY));
-    context.setLineDash([4, 4]);
-    console.log(x, y);
-    context.lineTo(x, (y += 100));
-    context.lineTo((x -= 50), (y += 30));
-    context.moveTo((x += 50), (y -= 30));
-    context.lineTo((x += 100), y);
-
-    // stroke dotted interior line
-    context.stroke();
+      // stroke the solid outline of the cube
+      context.stroke();
+    };
+    context.save(); // save current state onto a stack
+    context.translate(startingX, startingY);
+    drawCube();
+    context.restore();
   };
-
-  // context.save(); // save current state onto a stack
-  // drawCube(dx, dy);
-  drawCube(70, 40);
-};
-
-window.onload = () => {
+  s1.addEventListener("input", draw);
+  s2.addEventListener("input", draw);
   draw();
 };
+
+window.onload = setup;
