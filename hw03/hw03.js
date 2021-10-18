8d4f623e4222844490c8602c36568da23fb3251c
Andy Lin <lin383@wisc.edu>
2021-10-17 20:58:19 -0500

    added pants onto canvas

--- hw03/hw03.js
+++ hw03/hw03.js
@@ -1,25 +1,105 @@
 function setup() {
   var canvas = document.getElementById("hw03Canvas");
-  var cxt = canvas.getContext("2d");
+  var ctx = canvas.getContext("2d");
 
   function draw() {
-    cxt.clearRect(0, 0, canvas.width, canvas.height); // clear canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height); // clear canvas
 
+    // colors
+    const pantsColor = "#f1f388";
+    const shirtColor = "#ce4f4b";
+    const skinColor = "#e8b89c";
+    const socksColor = "#ffffff";
+    const shoesColor = "#eec752";
+
+    // fucntions borrowed from week4/demo1/demo.js
     var stack = [mat3.create()];
 
+    function moveToTx(x, y) {
+      var res = vec2.create();
+      vec2.transformMat3(res, [x, y], stack[0]);
+      ctx.moveTo(res[0], res[1]);
+    }
+
+    function lineToTx(x, y) {
+      var res = vec2.create();
+      vec2.transformMat3(res, [x, y], stack[0]);
+      ctx.lineTo(res[0], res[1]);
+    }
+
+    // helper function
+    // reset line settings back to default
+    function resetSetting() {
+      ctx.strokeStyle = "#000";
+      ctx.lineWidth = 1;
+      ctx.fillStyle = "#000";
+      ctx.lineJoin = "miter";
+    }
+
+    // draw outer circle (the path for Shin-chan to move around)
     function drawCircle() {
       var r = 190;
-      cxt.beginPath();
-      cxt.arc(canvas.width / 2, canvas.height / 2, r, 0, 2 * Math.PI);
-      cxt.stroke();
+      ctx.beginPath();
+      ctx.arc(canvas.width / 2, canvas.height / 2, r, 0, 2 * Math.PI);
+      ctx.stroke();
     }
 
     function drawShinChan() {
+      // line setting
+      ctx.lineWidth = 2;
+      ctx.lineJoin = "round";
+      ctx.fillStyle = pantsColor;
       // draw pants
+      ctx.beginPath();
+      moveToTx(0, 0);
+      lineToTx(170, 0);
+      lineToTx(173, 8);
+      lineToTx(173, 8);
+      lineToTx(171, 16);
+      lineToTx(173, 20);
+      lineToTx(171, 24);
+      lineToTx(180, 45);
+      lineToTx(90, 45);
+      lineToTx(85, 35);
+      lineToTx(80, 45);
+      lineToTx(-10, 45);
+      lineToTx(-1, 24);
+      lineToTx(-3, 20);
+      lineToTx(-1, 16);
+      lineToTx(-3, 8);
+      ctx.closePath();
+      ctx.fill();
+      ctx.stroke();
+
+      // draw left creases on pants
+      ctx.lineWidth = 1;
+      ctx.beginPath();
+      moveToTx(10, 10);
+      lineToTx(20, 15);
+      ctx.stroke();
+      ctx.beginPath();
+      moveToTx(7, 17);
+      lineToTx(15, 25);
+      ctx.stroke();
+
+      // draw right creases on pants
+      ctx.beginPath();
+      moveToTx(165, 16);
+      lineToTx(158, 19);
+      ctx.stroke();
+      ctx.beginPath();
+      moveToTx(167, 20);
+      lineToTx(155, 30);
+      ctx.stroke();
     }
 
     function main() {
       drawCircle();
+
+      var shinChanToCanvas = mat3.create();
+      mat3.fromTranslation(shinChanToCanvas, [120, 300]);
+      mat3.multiply(stack[0], stack[0], shinChanToCanvas);
+      drawShinChan();
     }
 
     main();
